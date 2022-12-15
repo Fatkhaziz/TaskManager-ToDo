aecdf035913b22fc98c5e1448537a32a294b46be
Shuhrat Muhamedov <shuhratmuhamedov@MacBook-Pro-Shuhrat.local>
2022-12-15 17:56:10 +0600

    Initial commit, finished to-do list project

---
+++ taskmanager/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField('Name', max_length=50)
+    task = models.TextField('Descriptiion', max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
